19933649b0fab8c72dce08651d634491a058a1f7
Andrew Crudge <andcru@gmail.com>
2013-04-05 00:49:39 -0400

    Cleanup

--- daqtest.js
+++
@@ -1,7 +0,0 @@
-var Led = require('/usr/local/lib/node_modules/pi-led/build/Release/PiLed.node').PiLed;
-
-var led = new Led();
-
-led.Cookies("Hi", function(err, result) {
-
-});
--- node.js
+++
@@ -1,7 +0,0 @@
-var hello = require('/var/www/node/node_modules/ReadSPI/build/Release/ReadSPI.node');
- 
-var str;
-setInterval(function() {
-	str = eval(hello.read()); // hello world
-	console.log(str);
-},100);
